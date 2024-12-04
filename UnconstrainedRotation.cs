77039355944b16232f9680afedc7a829c896f5f1
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2024-12-04 17:07:13 -0600

    Made constraint more persistent

--- UnconstrainedRotation.cs
+++ UnconstrainedRotation.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 namespace UnconstrainedRotation {
     public class UnconstrainedRotation: Mod {
         new public string GetName() => "UnconstrainedRotation";
-        public override string GetVersion() => "1.0.0.0";
+        public override string GetVersion() => "1.0.1.0";
 
         public override void Initialize() {
-            On.HeroController.Start += heroStart;
+            On.HeroController.Update10 += heroUpdate;
         }
 
-        private void heroStart(On.HeroController.orig_Start orig, HeroController self) {
+        private void heroUpdate(On.HeroController.orig_Update10 orig, HeroController self) {
             orig(self);
             self.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
         }
