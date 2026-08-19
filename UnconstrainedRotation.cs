3921c02490beb61f2a90e52bf488e631883527de
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2026-08-19 03:12:32 -0500

    Added togglability

--- UnconstrainedRotation.cs
+++ UnconstrainedRotation.cs
@@ -2,12 +2,22 @@
 using UnityEngine;
 
 namespace UnconstrainedRotation {
-    public class UnconstrainedRotation: Mod {
+    public class UnconstrainedRotation: Mod, ITogglableMod {
         new public string GetName() => "UnconstrainedRotation";
-        public override string GetVersion() => "1.0.2.0";
+        public override string GetVersion() => "1.1.0.0";
 
         public override void Initialize() {
             On.HeroController.Update10 += heroUpdate;
+        }
+
+        public void Unload() {
+            On.HeroController.Update10 -= heroUpdate;
+            try {
+                Rigidbody2D rb = HeroController.instance.gameObject.GetComponent<Rigidbody2D>();
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                rb.transform.rotation = Quaternion.identity;
+            }
+            catch(System.Exception) { }
         }
 
         private void heroUpdate(On.HeroController.orig_Update10 orig, HeroController self) {
